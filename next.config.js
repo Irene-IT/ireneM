8f48484470db19aa72c0befe23da2db10ba26c2e
Irene <irene.dev.sp@gmail.com>
2025-09-10 23:41:19 +0200

    fix config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
 // @ts-check
-
-/**
- * @type {import('next').NextConfig}
- */
-
 const { withContentlayer } = require('next-contentlayer2');
 
+/** @type {import('next').NextConfig} */
 const nextconfig = {
   reactStrictMode: true,
   trailingSlash: true,
-  // output: "export",
   images: {
     loader: "custom",
     imageSizes: [16, 32, 48, 128, 256],
@@ -23,17 +18,9 @@ const nextconfig = {
     nextImageExportOptimizer_quality: "75",
     nextImageExportOptimizer_storePicturesInWEBP: "true",
     nextImageExportOptimizer_exportFolderName: "nextImageExportOptimizer",
-
-    // If you do not want to use blurry placeholder images, then you can set
-    // nextImageExportOptimizer_generateAndUseBlurImages to false and pass
-    // `placeholder="empty"` to all <ExportedImage> components.
     nextImageExportOptimizer_generateAndUseBlurImages: "true",
-
-    // If you want to cache the remote images, you can set the time to live of the cache in seconds.
-    // The default value is 0 seconds.
     nextImageExportOptimizer_remoteImageCacheTTL: "0",
   },
 }
-
 
 module.exports = withContentlayer(nextconfig)
