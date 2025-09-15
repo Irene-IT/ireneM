21dc833a913fc530329f6ccaf4e05d325b081cba
Irene <irene.dev.sp@gmail.com>
2025-09-15 12:34:27 +0200

    add seo for section services

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -10,9 +10,9 @@ import { SITE_NAME, SITE_URL } from '../../config'
 
 export function generateMetadata(): Metadata {
   const SEO = {
-    title: 'Code Blog by Nuno Marques | Design & Development Tips',
+    title: 'B2B Content Writing Services | Articles, Blogs & White Papers',
     description:
-      'Discover the latest blogs on code, web design and development. Stay up to date with the latest trends and technologies, with code examples.',
+      'Boost your business growth with expert B2B content writing services. I create engaging articles, blogs, guides, and white papers that highlight your cloud computing, finance, and technology solutions',
     image: `${SITE_URL}/og-card.png`,
   }
 
@@ -20,7 +20,7 @@ export function generateMetadata(): Metadata {
     title: SEO.title,
     description: SEO.description,
     openGraph: {
-      url: `${SITE_URL}/blog/`,
+      url: `${SITE_URL}/services/`,
       title: SEO.title,
       description: SEO.description,
       images: [
@@ -64,7 +64,7 @@ export default async function BlogPage({
   return (
     <Layout>
       <section className="md:max-w-[87%] m-auto flex flex-col gap-6 px-4 sm:px-12 mb-32">
-        <CategoryHeader title="Code Blog" templateKey={blogs[0].templateKey!} />
+        <CategoryHeader title="Services" templateKey={blogs[0].templateKey!} />
 
         <div className="flex gap-8 items-start">
           <div className="flex flex-wrap gap-4 w-full">
