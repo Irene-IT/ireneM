14401a436c9a4da0e06708467c2921b02bc60615
Irene <irene.dev.sp@gmail.com>
2025-10-06 17:18:17 +0200

    add screen

--- next-seo.config.js
+++ next-seo.config.js
@@ -4,5 +4,15 @@ export default {
     locale: 'en',
     url: 'https://irenemyronova.netlify.app',
     site_name: 'Irene Myronova | B2B SaaS Content Writer',
+    title: 'Professional B2B content writer specializing in SaaS, IT and tech',
+    description: 'I help B2B SaaS brands grow with clear, compelling content that converts.',
+    images: [
+      {
+        url: 'https://irenemyronova.netlify.app/og-image.jpg',
+        width: 1600,
+        height: 800,
+        alt: 'Irene Myronova Portfolio',
+      },
+    ],
   },
 };
